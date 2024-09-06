01af9af8483ff1a107241ba1a8272222a93b46b3
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-06 17:08:12 -0300

    Configuração swagger

---
+++ src/main/java/com/observatudo/backend/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.observatudo.backend.config;
+
+//import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
