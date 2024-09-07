35583201bae6533e56ebb0785c975208df9b733d
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-07 12:01:32 -0300

    Adiciona metodo para buscar estado e capital

--- src/main/java/com/observatudo/backend/domain/repository/CidadeRepository.java
+++ src/main/java/com/observatudo/backend/domain/repository/CidadeRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.observatudo.backend.domain.model.Cidade;
+import com.observatudo.backend.domain.model.Estado;
 
 @Repository
 public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+    Cidade findByEstadoAndCapital(Estado estado, boolean capital);
     Cidade findByCodigo(Integer codigo);
 }
