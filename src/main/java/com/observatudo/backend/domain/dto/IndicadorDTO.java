79802a7154a20c62f5a6683c9796f6bd263810d8
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-08 19:49:54 -0300

    quick fix

--- src/main/java/com/observatudo/backend/domain/dto/IndicadorDTO.java
+++ src/main/java/com/observatudo/backend/domain/dto/IndicadorDTO.java
@@ -2,9 +2,9 @@ package com.observatudo.backend.domain.dto;
 
 public class IndicadorDTO {
 
-    private String id;
-    private String nome;
-    private String valor;
+    // private String id;
+    // private String nome;
+    // private String valor;
 
     // Getters e Setters
 }
