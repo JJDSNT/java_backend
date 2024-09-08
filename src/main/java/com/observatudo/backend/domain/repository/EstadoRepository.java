0dd31301ec34a87c7f8a68e8a323e85d82fc5d86
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-08 16:19:52 -0300

    Adiciona metodo para  listar estados de um país

--- src/main/java/com/observatudo/backend/domain/repository/EstadoRepository.java
+++ src/main/java/com/observatudo/backend/domain/repository/EstadoRepository.java
@@ -1,11 +1,15 @@
 package com.observatudo.backend.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.observatudo.backend.domain.model.Estado;
+import com.observatudo.backend.domain.model.Pais;
 
 @Repository
 public interface EstadoRepository extends JpaRepository<Estado, Integer> {
     Estado findByCodigo(Integer codigo);
+    List<Estado> findByPais(Pais pais);
 }
