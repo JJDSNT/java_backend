5c2499818c3147ab3f7ebf2083c96e64a0b1790a
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-08 17:10:51 -0300

    atualiza o codigo estadoDTO

--- src/main/java/com/observatudo/backend/service/LocalidadeService.java
+++ src/main/java/com/observatudo/backend/service/LocalidadeService.java
@@ -6,9 +6,12 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.observatudo.backend.domain.dto.CidadeDTO;
 import com.observatudo.backend.domain.dto.EstadoDTO;
 import com.observatudo.backend.domain.mapper.EntityMapper;
+import com.observatudo.backend.domain.model.Cidade;
 import com.observatudo.backend.domain.model.Estado;
+import com.observatudo.backend.domain.repository.CidadeRepository;
 import com.observatudo.backend.domain.repository.EstadoRepository;
 
 @Service
@@ -18,12 +21,22 @@ public class LocalidadeService {
     private EstadoRepository estadoRepository;
 
     @Autowired
+    private CidadeRepository cidadeRepository;
+
+    @Autowired
     private EntityMapper entityMapper;
 
     public List<EstadoDTO> listarEstadosComCidades() {
         List<Estado> estados = estadoRepository.findAll();
         return estados.stream()
-                .map(entityMapper::estadoToEstadoDTO)
+                .map(estado -> {
+                    List<Cidade> cidades = cidadeRepository.findByEstado(estado);
+                    List<CidadeDTO> cidadeDTOs = entityMapper.cidadeListToCidadeDTOList(cidades);
+
+                    // Atualizar o EstadoDTO para incluir a lista de cidades convertidas
+                    return new EstadoDTO(estado.getCodigo(), estado.getNome(), estado.getSigla(), cidadeDTOs);
+                })
                 .collect(Collectors.toList());
     }
+
 }
