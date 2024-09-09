65f4d648fa8a623e50c25e9f858125cd50f9a50f
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-09 20:38:33 -0300

    Refactor repository interfaces to include custom query methods

--- src/main/java/com/observatudo/backend/domain/repository/IndicadorRepository.java
+++ src/main/java/com/observatudo/backend/domain/repository/IndicadorRepository.java
@@ -1,13 +1,19 @@
 package com.observatudo.backend.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.observatudo.backend.domain.model.Indicador;
+import com.observatudo.backend.domain.model.Localidade;
 
 @Repository
 public interface IndicadorRepository extends JpaRepository<Indicador, Integer> {
+    // Métodos de consulta personalizados podem ser definidos aqui, se necessário
 
     Indicador findByNome(String nomeIndicador);
-    // Métodos de consulta personalizados podem ser definidos aqui, se necessário
+
+    List<Indicador> findByLocalidade(Localidade localidade);
+
 }
--- src/main/java/com/observatudo/backend/domain/repository/LocalidadeRepository.java
+++ src/main/java/com/observatudo/backend/domain/repository/LocalidadeRepository.java
@@ -8,5 +8,6 @@ import com.observatudo.backend.domain.model.Localidade;
 @Repository
 public interface LocalidadeRepository extends JpaRepository<Localidade, Integer> {
     // Métodos de consulta personalizados podem ser definidos aqui, se necessário
+    Localidade findById(Long id);
     Localidade findByCodigo(int codigo);
 }
