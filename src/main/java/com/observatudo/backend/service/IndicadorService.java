dc196857761e93734875972ec9a14147d16f9cab
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-09 20:17:23 -0300

    Refactor IndicadorService to use IndicadorRepository for retrieving and converting indicadores

--- src/main/java/com/observatudo/backend/service/IndicadorService.java
+++ src/main/java/com/observatudo/backend/service/IndicadorService.java
@@ -1,22 +1,38 @@
 package com.observatudo.backend.service;
 
 import com.observatudo.backend.domain.dto.IndicadorDTO;
+import com.observatudo.backend.domain.model.Indicador;
+import com.observatudo.backend.domain.repository.IndicadorRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class IndicadorService {
 
-    private List<IndicadorDTO> indicadores = new ArrayList<>();
+    @Autowired
+    private IndicadorRepository indicadorRepository;
 
     public List<IndicadorDTO> listarIndicadores() {
-        return indicadores;
+        List<Indicador> indicadores = indicadorRepository.findAll();
+        return indicadores.stream()
+                           .map(this::convertToDTO)
+                           .toList();
+    }
+
+    // private IndicadorDTO convertToDTO(Indicador indicador) {
+    //     return new IndicadorDTO(indicador.getFonte().getNome(),indicador.getCodIndicador(), indicador.getNome(), indicador.getDescricao());
+    // }
+
+    private IndicadorDTO convertToDTO(Indicador indicador) {
+        String fonteNome = indicador.getFonte() != null ? indicador.getFonte().getNome() : null;
+        return new IndicadorDTO(fonteNome, indicador.getCodIndicador(), indicador.getNome(), indicador.getDescricao());
     }
 
     // public IndicadorDTO adicionarIndicador(IndicadorDTO indicadorDTO) {
-    //     indicadores.add(indicadorDTO);
-    //     return indicadorDTO;
+    // indicadores.add(indicadorDTO);
+    // return indicadorDTO;
     // }
 }
