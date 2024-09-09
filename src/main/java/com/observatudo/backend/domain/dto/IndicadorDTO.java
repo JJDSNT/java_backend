8c609747a1a7185963c49e7be3b2c296be171ff4
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-09 20:17:36 -0300

    Refactor IndicadorDTO to include fonteNome, codIndicador, nome, and descricao properties

--- src/main/java/com/observatudo/backend/domain/dto/IndicadorDTO.java
+++ src/main/java/com/observatudo/backend/domain/dto/IndicadorDTO.java
@@ -2,9 +2,52 @@ package com.observatudo.backend.domain.dto;
 
 public class IndicadorDTO {
 
-    // private String id;
-    // private String nome;
-    // private String valor;
+    private String fonteNome;
+    private String codIndicador;
+    private String nome;
+    private String descricao;
 
-    // Getters e Setters
+    public IndicadorDTO() {
+        // Construtor padrão
+    }
+
+    public IndicadorDTO(String fonteNome, String codIndicador, String nome, String descricao) {
+        this.fonteNome = fonteNome;
+        this.codIndicador = codIndicador;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public String getFonteNome() {
+        return fonteNome;
+    }
+
+    public void setFonteNome(String fonteNome) {
+        this.fonteNome = fonteNome;
+    }
+
+    public String getCodIndicador() {
+        return codIndicador;
+    }
+
+    public void setCodIndicador(String codIndicador) {
+        this.codIndicador = codIndicador;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
 }
