d8bcee5b5347b55943465b7f30ec3813a6c93b35
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-09 20:57:30 -0300

    Refactor ResumoIndicadorService to use IndicadorRepository for retrieving indicators

--- src/main/java/com/observatudo/backend/service/ResumoIndicadorService.java
+++ src/main/java/com/observatudo/backend/service/ResumoIndicadorService.java
@@ -1,16 +1,10 @@
 package com.observatudo.backend.service;
 
+import com.observatudo.backend.domain.dto.IndicadorDTO;
 import com.observatudo.backend.domain.dto.ResumoIndicadorDTO;
-import com.observatudo.backend.domain.model.Cidade;
 import com.observatudo.backend.domain.model.Eixo;
-import com.observatudo.backend.domain.model.Estado;
 import com.observatudo.backend.domain.model.Indicador;
-import com.observatudo.backend.domain.model.Localidade;
-import com.observatudo.backend.domain.model.Pais;
-import com.observatudo.backend.domain.repository.EixoRepository;
 import com.observatudo.backend.domain.repository.IndicadorRepository;
-import com.observatudo.backend.domain.repository.LocalidadeRepository;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -24,9 +18,6 @@ public class ResumoIndicadorService {
 
     @Autowired
     private IndicadorRepository indicadorRepository;
-
-    @Autowired
-    private EixoRepository eixoRepository;
 
     public ResumoIndicadorDTO obterResumoIndicadores(Integer codigoLocalidade) {
         // Obtém os indicadores para a localidade especificada
