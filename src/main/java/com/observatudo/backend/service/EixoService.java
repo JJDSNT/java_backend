5ae51c04fdc99ca21c9762b5ec4d25b56d09bf21
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-09 23:31:20 -0300

    Refactor EixoService to add EixoPadrao functionality

--- src/main/java/com/observatudo/backend/service/EixoService.java
+++ src/main/java/com/observatudo/backend/service/EixoService.java
@@ -1,6 +1,8 @@
 package com.observatudo.backend.service;
 
 import com.observatudo.backend.domain.model.Eixo;
+import com.observatudo.backend.domain.model.EixoPadrao;
+import com.observatudo.backend.domain.repository.EixoPadraoRepository;
 import com.observatudo.backend.domain.repository.EixoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,9 @@ public class EixoService {
 
     @Autowired
     private EixoRepository eixoRepository;
+
+    @Autowired
+    private EixoPadraoRepository eixoPadraoRepository;
 
     public Eixo createEixo(Eixo eixo) {
         return eixoRepository.save(eixo);
@@ -27,5 +32,10 @@ public class EixoService {
 
     public boolean areEixosLoaded() {
         return eixoRepository.count() > 0;
+    }
+
+    public EixoPadrao getDefaultEixo() {
+        return eixoPadraoRepository.findSingletonEixoPadrao()
+                .orElseThrow(() -> new RuntimeException("Eixo padrão não encontrado"));
     }
 }
