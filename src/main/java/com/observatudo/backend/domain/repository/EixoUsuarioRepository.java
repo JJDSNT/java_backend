484bbd974249497bcd9ae17f08a5c8ff6625ac02
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-09 23:31:44 -0300

    Refactor EixoUsuarioRepository to add custom query for finding Eixo by UsuarioId

--- src/main/java/com/observatudo/backend/domain/repository/EixoUsuarioRepository.java
+++ src/main/java/com/observatudo/backend/domain/repository/EixoUsuarioRepository.java
@@ -1,12 +1,16 @@
 package com.observatudo.backend.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.observatudo.backend.domain.model.Eixo;
 import com.observatudo.backend.domain.model.EixoUsuario;
 
 @Repository
 public interface EixoUsuarioRepository extends JpaRepository<EixoUsuario, Integer> {
     // Métodos de consulta personalizados podem ser definidos aqui, se necessário
+        Optional<Eixo> findByUsuarioId(Long usuarioId);
 }
 
