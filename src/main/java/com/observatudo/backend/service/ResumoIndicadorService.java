0ee065602d625279fc705b144b2115bf95312cc4
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-09 21:22:11 -0300

    Refactor ResumoIndicadorService to use IndicadorService for retrieving indicators by localidadeCodigo

--- src/main/java/com/observatudo/backend/service/ResumoIndicadorService.java
+++ src/main/java/com/observatudo/backend/service/ResumoIndicadorService.java
@@ -4,24 +4,27 @@ import com.observatudo.backend.domain.dto.IndicadorDTO;
 import com.observatudo.backend.domain.dto.ResumoIndicadorDTO;
 import com.observatudo.backend.domain.model.Eixo;
 import com.observatudo.backend.domain.model.Indicador;
-import com.observatudo.backend.domain.repository.IndicadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 @Service
 public class ResumoIndicadorService {
 
+    private final IndicadorService indicadorService;
+
     @Autowired
-    private IndicadorRepository indicadorRepository;
+    public ResumoIndicadorService(IndicadorService indicadorService) {
+        this.indicadorService = indicadorService;
+    }
 
     public ResumoIndicadorDTO obterResumoIndicadores(Integer codigoLocalidade) {
         // Obtém os indicadores para a localidade especificada
-        List<Indicador> indicadores = indicadorRepository.findByLocalidadeCodigo(codigoLocalidade);
+        List<Indicador> indicadores = indicadorService.findByLocalidadeCodigo(codigoLocalidade);
 
         // Agrupa indicadores por eixo
         Map<Eixo, List<IndicadorDTO>> agrupadosPorEixo = new HashMap<>();
