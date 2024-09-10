46db09f23b67b0b6816cc9c9b4f96078fc812da3
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-10 10:49:38 -0300

    Refactor SwaggerConfig to remove unused import and update package name

--- src/main/java/com/observatudo/backend/config/SwaggerConfig.java
+++ src/main/java/com/observatudo/backend/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.observatudo.backend.config;
 
-//import org.springdoc.core.GroupedOpenApi;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
