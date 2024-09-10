4e2fc4efbc4d738871263b7d8796649f83273fd8
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-09 22:56:48 -0300

    Refactor EixoInitializer to update eixos initialization and add logging

--- src/main/java/com/observatudo/backend/initializer/EixoInitializer.java
+++ src/main/java/com/observatudo/backend/initializer/EixoInitializer.java
@@ -3,7 +3,8 @@ package com.observatudo.backend.initializer;
 import com.observatudo.backend.domain.model.Eixo;
 import com.observatudo.backend.domain.model.Eixos;
 import com.observatudo.backend.service.EixoService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -13,21 +14,32 @@ import java.util.List;
 @Component
 public class EixoInitializer {
 
-    @Autowired
-    private EixoService eixoService;
+    private static final Logger logger = LoggerFactory.getLogger(EixoInitializer.class);
+
+    private final EixoService eixoService;
+
+    public EixoInitializer(EixoService eixoService) {
+        this.eixoService = eixoService;
+    }
 
     @PostConstruct
     public void initialize() {
         if (!eixoService.areEixosLoaded()) {
             List<Eixo> eixos = Arrays.asList(
                 new Eixo(Eixos.SAUDE, "Saúde", "Saúde", "icon1", "cor1"),
-                new Eixo(Eixos.EDUCACAO, "Educação", "Educação", "icon2", "cor2")
-                // Adicione outros eixos conforme necessário
+                new Eixo(Eixos.EDUCACAO, "Educação", "Educação", "icon2", "cor2"),
+                new Eixo(Eixos.ASSISTENCIA_SOCIAL, "Assistência Social", "Assistência Social", "icon5", "cor5"),
+                new Eixo(Eixos.SEGURANCA, "Segurança", "Segurança", "icon4", "cor4"),
+                new Eixo(Eixos.MEIO_AMBIENTE, "Meio Ambiente", "Meio Ambiente", "icon6", "cor6"),
+                new Eixo(Eixos.ECONOMIA, "Economia", "Economia", "icon3", "cor3"),
+                new Eixo(Eixos.GOVERNANCA, "Governança", "Governança", "icon7", "cor7"),
+                new Eixo(Eixos.PERSONALIZADO, "Personalizado", "Personalizado", "icon8", "cor8")
             );
 
             eixoService.createEixos(eixos);
+            logger.info("Eixos carregados com sucesso no banco de dados.");
         } else {
-            System.out.println("Eixos já carregados no banco de dados.");
+            logger.info("Eixos já carregados no banco de dados.");
         }
     }
 }
