1170e77a49739f25725cc6937c9d00cc801be2e9
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-09 21:06:21 -0300

    Refactor IndicadorService to include methods for retrieving indicators by name and localidadeCodigo

--- src/main/java/com/observatudo/backend/service/IndicadorService.java
+++ src/main/java/com/observatudo/backend/service/IndicadorService.java
@@ -2,18 +2,24 @@ package com.observatudo.backend.service;
 
 import com.observatudo.backend.domain.dto.IndicadorDTO;
 import com.observatudo.backend.domain.model.Indicador;
+import com.observatudo.backend.domain.model.ValorIndicador;
 import com.observatudo.backend.domain.repository.IndicadorRepository;
+import com.observatudo.backend.domain.repository.ValorIndicadorRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class IndicadorService {
 
     @Autowired
     private IndicadorRepository indicadorRepository;
+
+    @Autowired
+    private ValorIndicadorRepository valorIndicadorRepository;
 
     public List<IndicadorDTO> listarIndicadores() {
         List<Indicador> indicadores = indicadorRepository.findAll();
@@ -22,17 +28,29 @@ public class IndicadorService {
                            .toList();
     }
 
-    // private IndicadorDTO convertToDTO(Indicador indicador) {
-    //     return new IndicadorDTO(indicador.getFonte().getNome(),indicador.getCodIndicador(), indicador.getNome(), indicador.getDescricao());
-    // }
 
     private IndicadorDTO convertToDTO(Indicador indicador) {
         String fonteNome = indicador.getFonte() != null ? indicador.getFonte().getNome() : null;
         return new IndicadorDTO(fonteNome, indicador.getCodIndicador(), indicador.getNome(), indicador.getDescricao());
     }
 
-    // public IndicadorDTO adicionarIndicador(IndicadorDTO indicadorDTO) {
-    // indicadores.add(indicadorDTO);
-    // return indicadorDTO;
-    // }
+    public IndicadorDTO buscarIndicadorPorNome(String nomeIndicador) {
+        Indicador indicador = indicadorRepository.findByNome(nomeIndicador);
+        return convertToDTO(indicador);
+    }
+
+    public List<Indicador> findIndicadoresByLocalidadeCodigo(Integer codigoLocalidade) {
+        List<ValorIndicador> valorIndicadores = valorIndicadorRepository.findByLocalidadeCodigo(codigoLocalidade);
+        List<String> codIndicadores = valorIndicadores.stream()
+                .map(valor -> valor.getIndicador().getCodIndicador())
+                .collect(Collectors.toList());
+
+        return indicadorRepository.findByFonteIdAndCodIndicadorIn(valorIndicadores.get(0).getIndicador().getFonte().getId(), codIndicadores);
+    }
 }
+
+
+
+
+
+
