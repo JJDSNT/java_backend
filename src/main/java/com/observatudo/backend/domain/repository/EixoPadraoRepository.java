059c597fb6875bc0520d33576d450643d9d7ec78
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-09 23:31:32 -0300

    Refactor EixoPadraoRepository to add custom query for finding singleton EixoPadrao

--- src/main/java/com/observatudo/backend/domain/repository/EixoPadraoRepository.java
+++ src/main/java/com/observatudo/backend/domain/repository/EixoPadraoRepository.java
@@ -1,6 +1,9 @@
 package com.observatudo.backend.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.observatudo.backend.domain.model.EixoPadrao;
@@ -8,4 +11,6 @@ import com.observatudo.backend.domain.model.EixoPadrao;
 @Repository
 public interface EixoPadraoRepository extends JpaRepository<EixoPadrao, Integer> {
     // Métodos de consulta personalizados podem ser definidos aqui, se necessário
+    @Query("SELECT ep FROM EixoPadrao ep")
+    Optional<EixoPadrao> findSingletonEixoPadrao();
 }
