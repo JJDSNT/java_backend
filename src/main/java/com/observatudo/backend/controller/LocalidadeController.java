8b4d9dc56da9a88f5a4bd0442f85260aeb07aeae
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-10 07:55:55 -0300

    Refactor LocalidadeController to include Swagger annotations for documenting API endpoints

--- src/main/java/com/observatudo/backend/controller/LocalidadeController.java
+++ src/main/java/com/observatudo/backend/controller/LocalidadeController.java
@@ -2,6 +2,9 @@ package com.observatudo.backend.controller;
 
 import com.observatudo.backend.domain.dto.EstadoDTO;
 import com.observatudo.backend.service.LocalidadeService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/localidades")
+@Tag(name = "Localidades", description = "Operações relacionadas a localidades")
 public class LocalidadeController {
 
     @Autowired
