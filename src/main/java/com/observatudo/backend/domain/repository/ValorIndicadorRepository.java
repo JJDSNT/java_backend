4b417bc939a25aeb1c1769bf1523e113da1a6913
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-09 21:06:06 -0300

    Refactor ValorIndicadorRepository to include custom query methods for retrieving values by localidadeCodigo and indicadorCodigo

--- src/main/java/com/observatudo/backend/domain/repository/ValorIndicadorRepository.java
+++ src/main/java/com/observatudo/backend/domain/repository/ValorIndicadorRepository.java
@@ -1,5 +1,7 @@
 package com.observatudo.backend.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.observatudo.backend.domain.model.ValorIndicador;
 @Repository
 public interface ValorIndicadorRepository extends JpaRepository<ValorIndicador, Long> {
     // Métodos de consulta personalizados podem ser definidos aqui, se necessário
+    List<ValorIndicador> findByLocalidadeCodigo(Integer codigoLocalidade);
+    List<ValorIndicador> findByIndicadorCodigo(Integer codigoIndicador);
 }
