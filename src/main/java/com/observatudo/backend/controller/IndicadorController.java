004be2025f1a5cad0d3553ab20176e1ef8479e9f
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-11 16:38:44 -0300

    Refactor IndicadorController to update Swagger annotations and API endpoint descriptions

--- src/main/java/com/observatudo/backend/controller/IndicadorController.java
+++ src/main/java/com/observatudo/backend/controller/IndicadorController.java
@@ -1,8 +1,9 @@
 package com.observatudo.backend.controller;
 
 import com.observatudo.backend.domain.dto.IndicadorDTO;
+import com.observatudo.backend.domain.dto.IndicadorValorDTO;
+import com.observatudo.backend.domain.dto.IndicadorValoresDTO;
 import com.observatudo.backend.domain.dto.LocalidadeIndicadoresDTO;
-import com.observatudo.backend.domain.dto.ResumoIndicadorDTO;
 import com.observatudo.backend.domain.model.Indicador;
 import com.observatudo.backend.service.IndicadorService;
 
@@ -14,7 +15,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,63 +38,50 @@ public class IndicadorController {
         this.indicadorService = indicadorService;
     }
 
-    @Operation(summary = "Listar todos os indicadores", description = "Retorna uma lista de todos os indicadores disponíveis.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Lista de indicadores retornada com sucesso",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = IndicadorDTO.class))),
-            @ApiResponse(responseCode = "500", description = "Erro no servidor", content = @Content)
-    })
-    @GetMapping
-    public List<IndicadorDTO> listarIndicadores() {
-        List<Indicador> indicadores = indicadorService.listarIndicadores();
-        return indicadores.stream()
-                .map(this::convertToDTO)
-                .collect(Collectors.toList());
-    }
+    // private IndicadorDTO convertToDTO(Indicador indicador) {
+    // String fonteNome = indicador.getFonte() != null ?
+    // indicador.getFonte().getNome() : null;
+    // return new IndicadorDTO(fonteNome, indicador.getCodIndicador(),
+    // indicador.getNome(), indicador.getDescricao());
+    // }
 
     private IndicadorDTO convertToDTO(Indicador indicador) {
-        String fonteNome = indicador.getFonte() != null ? indicador.getFonte().getNome() : null;
-        return new IndicadorDTO(fonteNome, indicador.getCodIndicador(), indicador.getNome(), indicador.getDescricao());
-    }
+        List<IndicadorValorDTO> valores = indicador.getValores().stream()
+                .map(valorIndicador -> new IndicadorValorDTO(valorIndicador.getData(), valorIndicador.getValor()))
+                .collect(Collectors.toList());
 
-    @Operation(summary = "Obter resumo de indicadores por localidade", description = "Retorna um resumo dos indicadores para a localidade especificada.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Resumo de indicadores retornado com sucesso",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResumoIndicadorDTO.class))),
-            @ApiResponse(responseCode = "404", description = "Localidade não encontrada", content = @Content),
-            @ApiResponse(responseCode = "500", description = "Erro no servidor", content = @Content)
-    })
-    @GetMapping("/{codigoLocalidade}")
-    public ResponseEntity<ResumoIndicadorDTO> obterResumoIndicadores(@PathVariable Integer codigoLocalidade) {
-        ResumoIndicadorDTO resumo = indicadorService.obterResumoIndicadores(codigoLocalidade);
-        return new ResponseEntity<>(resumo, HttpStatus.OK);
+        return new IndicadorDTO(indicador.getNome(), valores);
     }
 
     @Operation(summary = "Detalhamento de indicadores por localidade", description = "Retorna os indicadores e seus valores para o país, estado e cidade, dado o ID da cidade.")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Indicadores detalhados com sucesso",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = LocalidadeIndicadoresDTO.class))),
-        @ApiResponse(responseCode = "404", description = "Cidade não encontrada", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Erro no servidor", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Indicadores detalhados com sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = LocalidadeIndicadoresDTO.class))),
+            @ApiResponse(responseCode = "404", description = "Cidade não encontrada", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Erro no servidor", content = @Content)
     })
-    @GetMapping("/indicadores/{codigoCidade}")
-    public ResponseEntity<LocalidadeIndicadoresDTO> listarIndicadoresPorLocalidade(@PathVariable Integer codigoCidade) {
-        LocalidadeIndicadoresDTO indicadores = indicadorService.listarIndicadoresPorLocalidade(codigoCidade);
-        return ResponseEntity.ok(indicadores);
+
+    @GetMapping("/{codigoLocalidade}") // corrigir para que aceite outras localidades além de cidade
+    public ResponseEntity<LocalidadeIndicadoresDTO> listarIndicadoresPorLocalidade(
+            @PathVariable Integer codigoLocalidade) {
+
+        // Obter o objeto LocalidadeIndicadoresDTO do serviço
+        LocalidadeIndicadoresDTO localidadeIndicadores = indicadorService
+                .listarIndicadoresPorLocalidade(codigoLocalidade);
+
+        // Retornar o DTO completo, agrupando por cidade, estado e país
+        return ResponseEntity.ok(localidadeIndicadores);
     }
 
     @Operation(summary = "Filtrar indicadores por nome, fonte ou eixo", description = "Retorna os indicadores filtrados e agrupados por fonte e/ou eixo, com a possibilidade de filtrar pelo nome do indicador, nome da fonte ou eixo.")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Indicadores filtrados com sucesso",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = IndicadorDTO.class))),
-        @ApiResponse(responseCode = "500", description = "Erro no servidor", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Indicadores filtrados com sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = IndicadorDTO.class))),
+            @ApiResponse(responseCode = "500", description = "Erro no servidor", content = @Content)
     })
     @GetMapping("/filtro")
     public ResponseEntity<List<IndicadorDTO>> filtrarIndicadores(
-        @RequestParam(required = false) String nomeIndicador,
-        @RequestParam(required = false) String nomeFonte,
-        @RequestParam(required = false) String eixo
-    ) {
+            @RequestParam(required = false) String nomeIndicador,
+            @RequestParam(required = false) String nomeFonte,
+            @RequestParam(required = false) String eixo) {
         List<Indicador> indicadoresFiltrados = indicadorService.filtrarIndicadores(nomeIndicador, nomeFonte, eixo);
         List<IndicadorDTO> resultado = indicadoresFiltrados.stream()
                 .map(this::convertToDTO)
